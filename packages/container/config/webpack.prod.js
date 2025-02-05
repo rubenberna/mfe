4eb4ea267b15622fc9e98b0c9082176854985f43
rubenMacVFS <rubenberna.dev@gmail.com>
2025-02-05 17:50:09 +0100

    Added publicPath

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -8,9 +8,11 @@ const domain = process.env.PRODUCTION_DOMAIN; // this will be set during the CI/
 const prodConfig = {
   mode: 'production', // makes optimizations for production
   output: {
-    filename: '[name].[contenthash].js'
-  }, // ensures that whenever we build some file for production, all the different
-  // files will use this as a template to figure out how to name them. This is done primarily for caching issues
+    // ensures that whenever we build some file for production, all the different
+    // files will use this as a template to figure out how to name them. This is done primarily for caching issues
+    filename: '[name].[contenthash].js',
+    publicPath: '/container/latest/', // this is the path where the files will be hosted
+  },
   plugins: [
     new ModuleFederationPlugin({
       name: 'container',
