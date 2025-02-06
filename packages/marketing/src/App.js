1c9d1b6933b4d54c9e3c11d5f4b42548b0210347
rubenMacVFS <rubenberna.dev@gmail.com>
2025-02-06 13:04:45 +0100

    Added prefix namespace to prevent css class collision

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+// Whenever material ui generates the production classNames with a more or less random name, we want to prefix it
+// with 'ma' to prevent classname collisions
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma'
+})
+
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={Landing} />
